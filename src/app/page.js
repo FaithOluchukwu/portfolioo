3949ce1f4c28c835b27b7fe28fdf3ca924659f4a
FaithOluchukwu <158074861+FaithOluchukwu@users.noreply.github.com>
2024-05-08 06:49:14 -0400

    .

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ const Page = () => {
   return (
     <div className='block lg:flex gap-3'>
       <h2 className="mb-3 lg:text-black text-white  bg-blue-400 p-10">This is Tailwind</h2>
-      <h2> This is Tailwind</h2>
+      <h2 className="mb-3 lg:text-black text-white  bg-blue-400 p-10"> This is Tailwind</h2>
     </div>
   )
 }
